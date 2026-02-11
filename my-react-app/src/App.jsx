f3e57808f68041f5d14c022755937a41a634caaa
JKaren2025 <240097@utxicotepec.edu.mx>
2026-02-11 02:05:55 -0600

    Cambio
    Se cambió la paleta de colores frl fondo u tarjetas

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -9,7 +9,7 @@ import Estrellas from "./assets/Tarjetas/Estrellas.png";
 import Krampus from "./assets/Tarjetas/Krampus.png";
 import SDV from "./assets/Tarjetas/SDV.png";
 import Seccion6 from "./assets/Tarjetas/Seccion6.png";
-import Fondo from "./assets/Tarjetas/Fondo.jpg";
+const Fondo = "https://images2.alphacoders.com/132/1329303.jpeg";
 
 function App() {
 
@@ -19,11 +19,12 @@ function App() {
   // CONTENIDO POR SECCIÓN
   const contenido = {
     inicio: [
-      { img: "https://ih1.redbubble.net/image.5779472775.5983/flat,750x,075,f-pad,750x1000,f8f8f8.jpg", titulo: "Texto 1", texto: "Contenido de inicio 1" },
-      { img: "https://preview.redd.it/1-6-gonna-be-my-favorite-update-v0-3lwty3jtnvee1.jpeg?auto=webp&s=f4c5c176e2d64c41c3ea707334254c1bdc6ef1ca", titulo: "Texto 2", texto: "Contenido de inicio 2" },
-      { img: "https://ih1.redbubble.net/image.5591849458.1809/flat,750x1000,075,t.jpg", titulo: "Texto 3", texto: "Contenido de inicio 3" },
-      { img: "https://ih1.redbubble.net/image.5593131857.1264/flat,750x,075,f-pad,750x1000,f8f8f8.jpg", titulo: "Texto 4", texto: "Contenido de inicio 4" },
-      { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZG2USYwtQoadgKUTRXwTppoGUPnYgd02U5g&s", titulo: "Texto 5", texto: "Contenido de inicio 5" }
+      { img: "https://fastcdn.hoyoverse.com/content-v2/plat/124392/3ba9a74bbed3272ff0a14e861e1e8418_8660564063196114626.png", titulo: "Historia principal", texto: "La civilización contemporánea fue destruida por un desastre sobrenatural llamado \"Huecos\". Nueva Eridu logró sobrevivir gracias a su tecnología para extraer recursos valiosos..." },
+      { img: "https://zzz.honeyhunterworld.com/img/tutorial/999231001-tutorial_image.webp", titulo: "TOPS", texto: "Está formada por los conglomerados corporativos más poderosos e influyentes de Nueva Eridu. TOPS tiene un peso clave en casi todos los sectores urbanos y rivaliza con la familia Mayflower..." },
+      { img: "https://upload-os-bbs.hoyolab.com/upload/2024/08/23/401316945/c86276c2025c6b0cd57267f135cfb4cc_2293042323653874762.jpeg", titulo: "Cavidades", texto: "Las Cavidades son catástrofes esféricas que aparecen de la nada y devoran todo a su paso. En su interior se distorsionan el espacio y el tiempo, habitan seres etéreos y existe riesgo de corrupción y mutación..." },
+      { img: "https://fbi.cults3d.com/uploaders/18178453/illustration-file/40b71dd9-fb18-4b16-b901-c66a3bd718ff/Your-paragraph-text-2.png", titulo: "Facciones", texto: "Las facciones en Zenless Zone Zero reúnen agentes jugables, Combat Bangboo, enemigos y NPC, como corporaciones y organizaciones que comparten identidad, objetivo o afiliación..." },
+      { img: "https://pbs.twimg.com/media/HAvZtLnbIAADmVu.jpg", titulo: "Texto 5", texto: "Contenido de inicio 5" },
+      { img: "https://fastcdn.hoyoverse.com/content-v2/plat/124392/3ba9a74bbed3272ff0a14e861e1e8418_8660564063196114626.png", titulo: "Void Hunter", texto: "Cazador del Vacio es un prestigioso titulo otorgado por el alcalde a quienes logran grandes hazanas: proteger la ciudad frente a Huecos peligrosos o impulsar tecnologias revolucionarias..." }
     ],
 
     acerca: [
